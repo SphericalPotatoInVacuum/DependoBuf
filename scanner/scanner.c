d89920f608aeaae78f2edeb609a7c3a2201708a5
Alexander Bogdanov <a@bogdanov.co>
2023-02-10 13:53:52 +0500

    🎨 Slight rewording

--- scanner/scanner.c
+++ scanner/scanner.c
@@ -1,19 +1,29 @@
-#include <stdio.h>
-
 #include "lex/constants.h"
+
+#include <stdio.h>
 
 extern int yylex();
 extern int yylineno;
-extern char* yytext;
+extern char *yytext;
 
 int main(void) {
-    int token;
+  int token;
+  int prevline = -1;
 
-    token = yylex();
-    while (token) {
-        printf("%d\n", token);
-        token = yylex();
+  while ((token = yylex())) {
+    if (yylineno != prevline) {
+      if (prevline != -1)
+        printf("\n");
+      printf("%3d:", yylineno);
+      prevline = yylineno;
     }
+    printf(" (%d", token);
+    if (token >= LC_IDENTIFIER && token <= FLOAT) {
+      printf(": %s", yytext);
+    }
+    printf(")");
+  }
+  printf("\n");
 
-    return 0;
+  return 0;
 }
