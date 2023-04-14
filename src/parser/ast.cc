1319f3bd99e50d070deb9c1f4cbd65eba0b6f1ef
Alexander Bogdanov <a@bogdanov.co>
2023-04-15 02:11:54 +0500

    ✨ Introduce enums as a separate entity
    parser: modified ast and bison actions to properly handle enums
    tests: fixed issues introduced by syntax change, need to update

--- src/parser/ast.cc
+++ src/parser/ast.cc
@@ -1,31 +1,48 @@
-#include "ast.h"
+#include "parser/ast.h"
+
+#include <iostream>
 
 namespace dbuf::parser {
 
-void AST::AddMessage(std::unique_ptr<Message> &&message) {
-  messages_.insert(std::make_pair(message->name_, std::move(message)));
+TypedVariable::TypedVariable(std::string name_, std::unique_ptr<TypeExpression> type_expression)
+    : name(std::move(name_))
+    , type_expression(std::move(type_expression)) {}
+
+Constructor::Constructor(std::string name)
+    : name_(std::move(name)) {}
+
+void Constructor::AddField(std::unique_ptr<TypedVariable> field) {
+  fields_.emplace_back(std::move(field));
 }
 
-Message::Message(std::string &name)
-    : name_(name) {}
-
-void Message::AddInput(std::vector<std::unique_ptr<Value>> input) {
-  inputs_.emplace_back(std::move(input));
-}
-
-void Message::AddOutput(std::vector<std::unique_ptr<Constructor>> output) {
-  outputs_.emplace_back(std::move(output));
-}
+Message::Message(std::string name)
+    : name_(std::move(name)) {}
 
 void Message::AddDependency(std::unique_ptr<TypedVariable> type_dependency_) {}
 
-Field::Field(std::string &name, std::unique_ptr<TypeExpression> type_expression)
-    : name_(name)
-    , type_expression_(std::move(type_expression)) {}
+void Message::AddField(std::unique_ptr<TypedVariable> field) {
+  fields_.emplace_back(std::move(field));
+}
 
-Constructor::Constructor(std::string &name)
+Enum::Enum(std::string &name)
     : name_(name) {}
 
-void Constructor::AddField(std::unique_ptr<Field> field) { fields_.emplace_back(std::move(field)); }
+void Enum::AddDependency(std::unique_ptr<TypedVariable> type_dependency_) {}
+
+void Enum::AddInput(std::vector<std::unique_ptr<std::variant<Value, StarValue>>> input) {
+  inputs_.emplace_back(std::move(input));
+}
+
+void Enum::AddOutput(std::vector<std::unique_ptr<Constructor>> output) {
+  outputs_.emplace_back(std::move(output));
+}
+
+void AST::AddMessage(std::unique_ptr<Message> message) {
+  messages_.insert(std::make_pair(message->name_, std::move(message)));
+}
+
+void AST::AddEnum(std::unique_ptr<Enum> enum_) {
+  enums_.insert(std::make_pair(enum_->name_, std::move(enum_)));
+}
 
 } // namespace dbuf::parser
--- src/parser/ast.h
+++ src/parser/ast.h
@@ -1,54 +1,64 @@
 #pragma once
 
-#include "expression.h"
+#include "parser/expression.h"
 
 #include <memory>
 #include <string>
 #include <unordered_map>
+#include <variant>
 #include <vector>
 
 namespace dbuf::parser {
 
 struct TypedVariable {
-  TypedVariable(std::string name_, std::unique_ptr<TypeExpression> type_expression)
-      : name(std::move(name_))
-      , type_expression(std::move(type_expression)) {}
+  TypedVariable(std::string name_, std::unique_ptr<TypeExpression> type_expression);
   std::string name;
   std::unique_ptr<TypeExpression> type_expression;
 };
 
-struct Field {
-  explicit Field(std::string &name, std::unique_ptr<TypeExpression> type_expression);
-
-  std::string name_;
-  std::unique_ptr<TypeExpression> type_expression_;
-};
-
 struct Constructor {
-  explicit Constructor(std::string &name);
+  Constructor() = default;
+  explicit Constructor(std::string name);
 
-  void AddField(std::unique_ptr<Field> field);
+  void AddField(std::unique_ptr<TypedVariable> field);
 
   std::string name_;
-  std::vector<std::unique_ptr<Field>> fields_;
+  std::vector<std::unique_ptr<TypedVariable>> fields_ = {};
 };
 
 struct Message {
-  explicit Message(std::string &name);
-  void AddInput(std::vector<std::unique_ptr<Value>> input);
+  Message() = default;
+  explicit Message(std::string name);
+
   void AddDependency(std::unique_ptr<TypedVariable> type_dependency_);
+  void AddField(std::unique_ptr<TypedVariable> field);
+
+  std::string name_;
+  std::unordered_map<std::string, std::unique_ptr<TypedVariable>> type_dependencies_;
+  std::vector<std::unique_ptr<TypedVariable>> fields_ = {};
+};
+
+struct Enum {
+  Enum() = default;
+  explicit Enum(std::string &name);
+
+  void AddDependency(std::unique_ptr<TypedVariable> type_dependency_);
+
+  void AddInput(std::vector<std::unique_ptr<std::variant<Value, StarValue>>> input);
   void AddOutput(std::vector<std::unique_ptr<Constructor>> output);
 
   std::string name_;
   std::unordered_map<std::string, std::unique_ptr<TypedVariable>> type_dependencies_;
-  std::vector<std::vector<std::unique_ptr<Value>>> inputs_;
+  std::vector<std::vector<std::unique_ptr<std::variant<Value, StarValue>>>> inputs_;
   std::vector<std::vector<std::unique_ptr<Constructor>>> outputs_;
 };
 
 struct AST {
-  void AddMessage(std::unique_ptr<Message> &&message);
+  void AddMessage(std::unique_ptr<Message> message);
+  void AddEnum(std::unique_ptr<Enum> enum_);
 
   std::unordered_map<std::string, std::unique_ptr<Message>> messages_;
+  std::unordered_map<std::string, std::unique_ptr<Enum>> enums_;
 };
 
 } // namespace dbuf::parser
