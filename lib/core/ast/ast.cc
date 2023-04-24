a21afb4c1e76b3e15d4730198606576453c0fc74
Alexander Bogdanov <a@bogdanov.co>
2023-04-25 00:30:39 +0500

    ♻️ Improve readability and simplify grammar rules

--- lib/core/ast/ast.cc
+++ lib/core/ast/ast.cc
@@ -6,15 +6,15 @@
 namespace dbuf::ast {
 
 void AST::AddMessage(Message &&message_arg) {
-  messages_.insert(std::make_pair(message_arg.name_, std::move(message_arg)));
+  messages.insert(std::make_pair(message_arg.name, std::move(message_arg)));
 }
 
 void AST::AddEnum(Enum &&enum_arg) {
-  enums_.insert(std::make_pair(enum_arg.name_, std::move(enum_arg)));
+  enums.insert(std::make_pair(enum_arg.name, std::move(enum_arg)));
 }
 
 uint64_t AST::GetInterning(std::string &&input_string) {
-  return interning_.GetInterning(std::move(input_string));
+  return interning.GetInterning(std::move(input_string));
 }
 
 } // namespace dbuf::ast
