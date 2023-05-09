847909975df0d992db496354c2ec8d57041bacee
Alexander Bogdanov <a@bogdanov.co>
2023-05-09 20:01:11 +0500

    ✨ Add glog

--- src/main.cc
+++ src/main.cc
@@ -1,10 +1,12 @@
 #include "core/driver.h"
+#include "glog/logging.h"
 
 #include <cstdlib>
 #include <cstring>
 #include <iostream>
 
 int main(const int argc, const char **argv) {
+  google::InitGoogleLogging(argv[0]);
   if (argc == 3) {
     return dbuf::Driver::Run(argv[1], argv[2]);
   }
--- test/test.cc
+++ test/test.cc
@@ -1,6 +1,11 @@
+#include "glog/logging.h"
+
 #include <gtest/gtest.h>
 
 int main(int argc, char **argv) {
+  FLAGS_logtostderr = true;
+  google::InitGoogleLogging(argv[0]);
+
   ::testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
 }
