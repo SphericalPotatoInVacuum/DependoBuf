54349bb69af9ad38bd96ec00305820d985c407aa
Alisa Vernigor <vernigor.alisa@gmail.com>
2023-05-11 13:52:16 +0300

    Fix bug

--- lib/core/checker/include/core/checker/type_expression_checker.h
+++ lib/core/checker/include/core/checker/type_expression_checker.h
@@ -46,6 +46,10 @@ public:
     }
   }
 
+  // This case should not be reachable
+  // TODO(alisa-vernigor): return error
+  void operator()(const ast::Expression &expected_type, const ast::Expression &star);
+
   void operator()(const ast::Expression & /*expected_type*/, const ast::Star & /*star*/);
   void operator()(const ast::Expression & /*expected_type*/, const ast::Value & /*value*/);
 
