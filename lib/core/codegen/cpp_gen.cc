bd54447e86a0ef8f696d532c767e96bbb1dab3fc
P0lIg0rka <ivpolezhaev@edu.hse.ru>
2024-04-12 16:04:15 +0300

    CppCodeGenerator not const)

--- lib/core/codegen/cpp_gen.cc
+++ lib/core/codegen/cpp_gen.cc
@@ -1,7 +1,7 @@
 #include "core/codegen/generation.h"
 
 namespace dbuf::gen {
-void CppCodeGenerator::Generate(ast::AST *tree) const {
+void CppCodeGenerator::Generate(ast::AST *tree) {
   if (tree == nullptr) {
     std::cerr << "Cpp codegen did\'t get AST" << std::endl;
   }
