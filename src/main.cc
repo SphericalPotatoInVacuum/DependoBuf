d546d4b30a0ee41e381f1a7899c742f4cd7a49ef
P0lIg0rka <ivpolezhaev@edu.hse.ru>
2024-04-17 21:14:37 +0300

    Returned include

--- src/main.cc
+++ src/main.cc
@@ -8,6 +8,7 @@ it under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
 (at your option) any later version.
 */
+#include "CLI/CLI.hpp"
 #include "core/driver/driver.h"
 #include "glog/logging.h"
 
