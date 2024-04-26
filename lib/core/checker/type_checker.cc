3eea352558898f9d4bc7c9aacc6cef17394b98cf
Areg Alanyan <aregalanyan@gmail.com>
2024-04-26 18:34:58 +0000

    reterned back deleted code

--- lib/core/checker/type_checker.cc
+++ lib/core/checker/type_checker.cc
@@ -111,6 +111,78 @@ bool TypeChecker::operator()(const ast::Enum &ast_enum) {
       return false;
     }
 
+    auto scope = Scope(&context_);
+    substitutor_.PushScope();
+
+    // Check input patterns
+    for (size_t id = 0; id < rule.inputs.size(); ++id) {
+      // We ignore Stars as they match any type and do not declare new variables
+      if (std::holds_alternative<ast::Star>(rule.inputs[id])) {
+        continue;
+      }
+
+      const auto &value = std::get<ast::Value>(rule.inputs[id]);
+      substitutor_.AddSubstitution(ast_enum.type_dependencies[id].name, std::make_shared<const ast::Expression>(value));
+
+      // Check that value has expected type in this context
+      error_ = TypeComparator(ast_enum.type_dependencies[id].type_expression, ast_, &context_, &substitutor_, &z3_stuff_).Compare(ast::Expression(value));
+      if (error_.has_value()) {
+        return false;
+      }
+
+      // Now we need to add newly declared variables to the context
+
+      // Scalar values do not declare new variables, so we skip them
+      if (!std::holds_alternative<ast::ConstructedValue>(value)) {
+        continue;
+      }
+
+      const auto &constructed_value = std::get<ast::ConstructedValue>(value);
+
+      // Find constructor used to construct the value
+      InternedString type_name            = ast_enum.type_dependencies[id].type_expression.identifier.name;
+      const auto &type                    = ast_.types.at(type_name);
+      ast::TypeWithFields const *type_ptr = nullptr;
+      ast::Identifier identifier;
+      if (std::holds_alternative<ast::Enum>(type)) {
+        const auto &target_enum = std::get<ast::Enum>(type);
+
+        // Looking for the constructor
+        for (const auto &pattern : target_enum.pattern_mapping) {
+          for (const auto &output : pattern.outputs) {
+            if (output.identifier.name == constructed_value.constructor_identifier.name) {
+              type_ptr   = &output;
+              identifier = output.identifier;
+              break;
+            }
+          }
+        }
+      } else {
+        type_ptr   = &std::get<ast::Message>(type);
+        identifier = std::get<ast::Message>(type).identifier;
+      }
+
+      const auto &type_with_fields = *type_ptr;
+
+      if (type_with_fields.fields.size() != constructed_value.fields.size()) {
+        error_ = Error (CreateError() << "Expected " << type_with_fields.fields.size() << " fields in constructor \"" << identifier.name << "\", but got " << constructed_value.fields.size() << " at " << constructed_value.location);
+        return false;
+      }
+
+      // Adding new variables to scope
+      for (size_t j = 0; j < type_with_fields.fields.size(); ++j) {
+        if (!std::holds_alternative<ast::VarAccess>(*constructed_value.fields[j].second)) {
+          continue;
+        }
+        ast::VarAccess var_access = std::get<ast::VarAccess>(*constructed_value.fields[j].second);
+
+        // If new variable
+        if (var_access.field_identifiers.empty()) {
+          scope.AddName(var_access.var_identifier.name, type_with_fields.fields[j].type_expression);
+        }
+      }
+    }
+
     for (const auto &constructor : rule.outputs) {
       if (!CheckFields(constructor)) {
         return false;
