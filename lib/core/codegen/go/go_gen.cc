184cbf0629a0f28f0896137e5c61dc5649922298
Rustem Gimadiev <hrustim25@gmail.com>
2024-05-01 22:38:15 +0300

    codegen base

---
+++ lib/core/codegen/go/go_gen.cc
@@ -0,0 +1,7 @@
+#include "core/codegen/go/go_gen.h"
+
+namespace dbuf::gen {
+
+void GoCodeGenerator::Generate(ast::AST *tree) {}
+
+} // namespace dbuf::gen
---
+++ lib/core/codegen/include/core/codegen/go/go_gen.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "core/codegen/generation.h"
+
+namespace dbuf::gen {
+
+class GoCodeGenerator : public dbuf::gen::ITargetCodeGenerator {
+public:
+  explicit GoCodeGenerator(const std::string &out_file)
+      : ITargetCodeGenerator(out_file) {}
+
+  void Generate(ast::AST *tree) override;
+};
+
+} // namespace dbuf::gen
