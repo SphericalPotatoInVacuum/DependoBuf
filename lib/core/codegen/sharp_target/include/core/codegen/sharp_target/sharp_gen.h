9012af5483ff35346fea10548bc12006a71bfa7d
Anastasia Romanova <nastyarom30@gmail.com>
2024-05-14 19:33:33 +0000

    Fix clang for headers

--- lib/core/codegen/sharp_target/include/core/codegen/sharp_target/sharp_gen.h
+++ lib/core/codegen/sharp_target/include/core/codegen/sharp_target/sharp_gen.h
@@ -10,68 +10,68 @@
 namespace dbuf::gen {
 
 struct VectorHash {
-    std::size_t operator()(std::vector<std::pair<InternedString, InternedString>> &vec) const;
+  std::size_t operator()(std::vector<std::pair<InternedString, InternedString>> &vec) const;
 
-    std::size_t operator()(const std::vector<std::pair<InternedString, InternedString>> &vec) const;
+  std::size_t operator()(const std::vector<std::pair<InternedString, InternedString>> &vec) const;
 };
 
 class SharpCodeGenerator : public ITargetCodeGenerator {
 public:
-    explicit SharpCodeGenerator(const std::string &out_file);
-    
-    void Generate(const ast::AST *tree) override;
+  explicit SharpCodeGenerator(const std::string &out_file);
+
+  void Generate(const ast::AST *tree) override;
 
 private:
-    void operator()(const ast::Message &ast_message);
+  void operator()(const ast::Message &ast_message);
 
-    void operator()(const ast::Message &ast_message, const std::vector<ast::TypedVariable> &checker_input);
+  void operator()(const ast::Message &ast_message, const std::vector<ast::TypedVariable> &checker_input);
 
-    void operator()(const ast::Enum &ast_enum);
+  void operator()(const ast::Enum &ast_enum);
 
-    void operator()(const ast::Enum &ast_enum, const std::vector<ast::TypedVariable> &checker_input);
+  void operator()(const ast::Enum &ast_enum, const std::vector<ast::TypedVariable> &checker_input);
 
-    void operator()(const ast::TypedVariable &variable, bool as_dependency = false);
+  void operator()(const ast::TypedVariable &variable, bool as_dependency = false);
 
-    void operator()(const ast::Expression &expr, bool as_dependency = false, bool need_access = true);
+  void operator()(const ast::Expression &expr, bool as_dependency = false, bool need_access = true);
 
-    void operator()(const ast::TypeExpression &expr, bool as_dependency = false, bool need_access = true);
+  void operator()(const ast::TypeExpression &expr, bool as_dependency = false, bool need_access = true);
 
-    void operator()(const ast::BinaryExpression &expr);
+  void operator()(const ast::BinaryExpression &expr);
 
-    void operator()(const ast::UnaryExpression &expr);
+  void operator()(const ast::UnaryExpression &expr);
 
-    void operator()(const ast::ConstructedValue &value);
+  void operator()(const ast::ConstructedValue &value);
 
-    void operator()(const ast::Value &value);
+  void operator()(const ast::Value &value);
 
-    void operator()(const ast::VarAccess &var_access);
+  void operator()(const ast::VarAccess &var_access);
 
-    void operator()(const ast::Star &star);
+  void operator()(const ast::Star &star);
 
-    bool CheckForTriggers(
-        const std::unordered_set<InternedString> &trigger_names,
-        const ast::Expression &expr);
+  bool CheckForTriggers(
+    const std::unordered_set<InternedString> &trigger_names,
+    const ast::Expression &expr);
 
-    void PrintTypedVariables(
-        const std::vector<ast::TypedVariable> &variables,
-        std::string &&delimeter,
-        bool with_types,
-        bool add_last_delimeter,
-        bool as_dependency = false,
-        bool need_access = true);
-    
-    void PrintCheck(
-        const std::unordered_map<InternedString, std::vector<std::shared_ptr<const ast::Expression>>> &checker_members,
-        const std::vector<ast::TypedVariable> &checker_input
-    );
+  void PrintTypedVariables(
+    const std::vector<ast::TypedVariable> &variables,
+    std::string &&delimeter,
+    bool with_types,
+    bool add_last_delimeter,
+    bool as_dependency = false,
+    bool need_access = true);
+  
+  void PrintCheck(
+    const std::unordered_map<InternedString, std::vector<std::shared_ptr<const ast::Expression>>> &checker_members,
+    const std::vector<ast::TypedVariable> &checker_input
+  );
 
-    static bool IsSimpleType(const InternedString &interned_string);
+  static bool IsSimpleType(const InternedString &interned_string);
 
-    const ast::AST *tree_;
+  const ast::AST *tree_;
 
-    SharpPrinter printer_;
+  SharpPrinter printer_;
 
-    std::unordered_set<InternedString> created_hidden_types_;
+  std::unordered_set<InternedString> created_hidden_types_;
 };
 
 } // namespace dbuf::gen
--- lib/core/codegen/sharp_target/include/core/codegen/sharp_target/sharp_print.h
+++ lib/core/codegen/sharp_target/include/core/codegen/sharp_target/sharp_print.h
@@ -64,73 +64,73 @@ static const std::string kBaseEnumCheck = "\tpublic bool Check() {\n"
 
 class SharpTypes {
 public:
-    static constexpr std::string ConstructSharpType(const std::string &dbuf_type);
+  static constexpr std::string ConstructSharpType(const std::string &dbuf_type);
 
 private:
-    static constexpr unsigned int HashString(const std::string &str);
-    
-    static constexpr const char* kTypes[] = {"bool", "float", "int", "string", "unsigned"};
+  static constexpr unsigned int HashString(const std::string &str);
+
+  static constexpr const char* kTypes[] = {"bool", "float", "int", "string", "unsigned"};
 };
 
 class SharpPrinter {
 public:
-    SharpTypes type_constructor_;
+  SharpTypes type_constructor_;
 
-    SharpPrinter() = default;
+  SharpPrinter() = default;
 
-    explicit SharpPrinter(std::shared_ptr<std::ofstream> output);
+  explicit SharpPrinter(std::shared_ptr<std::ofstream> output);
 
-    void InitFile();
+  void InitFile();
 
-    void CompleteFile();
+  void CompleteFile();
 
-    void PrintClassBegin(const std::string &name);
+  void PrintClassBegin(const std::string &name);
 
-    void PrintClassEnd();
+  void PrintClassEnd();
 
-    void PrintConstructorBegin(
-        const std::string &name,
-        const std::vector<std::pair<InternedString, InternedString>> &dependent_variables,
-        bool print_vars = true);
-    
-    void PrintConstructorEnd();
+  void PrintConstructorBegin(
+    const std::string &name,
+    const std::vector<std::pair<InternedString, InternedString>> &dependent_variables,
+    bool print_vars = true);
+  
+  void PrintConstructorEnd();
 
-    void PrintBaseEnumFields();
+  void PrintBaseEnumFields();
 
-    void PrintBaseEnumConstructor();
+  void PrintBaseEnumConstructor();
 
-    void PrintBaseEnumCheck();
-    
-    void PrintTypeExpression(
-        const ast::TypeExpression &expression,
-        bool is_public,
-        bool as_dependency = false,
-        bool need_access = true);
-    
-    void PrintBinaryExpressionBegin();
+  void PrintBaseEnumCheck();
+  
+  void PrintTypeExpression(
+    const ast::TypeExpression &expression,
+    bool is_public,
+    bool as_dependency = false,
+    bool need_access = true);
+  
+  void PrintBinaryExpressionBegin();
 
-    void PrintBinaryExpressionType(const ast::BinaryExpression &binary_expr);
+  void PrintBinaryExpressionType(const ast::BinaryExpression &binary_expr);
 
-    void PrintBinaryExpressionEnd();
+  void PrintBinaryExpressionEnd();
 
-    void PrintUnaryExpressionType(const ast::UnaryExpression &unary_expr);
+  void PrintUnaryExpressionType(const ast::UnaryExpression &unary_expr);
 
-    void PrintConstructedValueBegin(const ast::ConstructedValue &constr_value);
+  void PrintConstructedValueBegin(const ast::ConstructedValue &constr_value);
 
-    void PrintConstructedValueEnd();
-    
-    void PrintValue(const ast::Value &value);
+  void PrintConstructedValueEnd();
+  
+  void PrintValue(const ast::Value &value);
 
-    void PrintVarAccess(const ast::VarAccess &var_access);
+  void PrintVarAccess(const ast::VarAccess &var_access);
 
 private:
-    static std::string GetVariableName(const ast::TypedVariable &var);
+  static std::string GetVariableName(const ast::TypedVariable &var);
 
-    static std::string GetVariableType(const ast::TypedVariable &var);
+  static std::string GetVariableType(const ast::TypedVariable &var);
 
-    static std::string GetExpressionType(const ast::TypeExpression &expr);
+  static std::string GetExpressionType(const ast::TypeExpression &expr);
 
-    std::shared_ptr<std::ofstream> out_ = nullptr;
+  std::shared_ptr<std::ofstream> out_ = nullptr;
 };
 
 } // namespace dbuf::gen
