54b2e64a9f53974119c235be0935ada258916757
Ilya Tkemaladze <intkemaladze@gmail.com>
2024-08-18 20:03:39 +0300

    remove debug output

--- lib/core/codegen/kotlin_target/kotlin_gen.cc
+++ lib/core/codegen/kotlin_target/kotlin_gen.cc
@@ -13,32 +13,15 @@ CodeGenerator::CodeGenerator(const std::string &out_file)
     , printer_(output_) {}
 
 void CodeGenerator::Generate(ast::AST *tree) {
-  std::cerr << "types:\n";
-  for (auto &[a, b] : tree->types) {
-    if (std::holds_alternative<ast::Message>(b)) {
-      std::cerr << "  Message [" << a << "]\n";
-    }
-    if (std::holds_alternative<ast::Enum>(b)) {
-      std::cerr << "  Enum [" << a << "]\n";
-    }
-  }
-  std::cerr << "constructors to type:\n";
-  for (auto &[a, b] : tree->constructor_to_type) {
-    std::cerr << "  " << a << " to " << b << "\n";
-  }
-  std::cerr << "visit order\n";
-  for (auto &a : tree->visit_order) {
-    std::cerr << "  " << a << "\n";
-  }
-
   for (const auto &t : tree->visit_order) {
     if (std::holds_alternative<ast::Message>(tree->types[t])) {
       const auto &message = std::get<ast::Message>(tree->types[t]);
       printer_ << PrintableMessage(message, tree);
-    }
-    if (std::holds_alternative<ast::Enum>(tree->types[t])) {
+    } else if (std::holds_alternative<ast::Enum>(tree->types[t])) {
       const auto &ast_enum = std::get<ast::Enum>(tree->types[t]);
       printer_ << PrintableEnum(ast_enum, tree);
+    } else {
+      throw std::string("kotlin code generation: unknow variant of AST.types.second");
     }
   }
 }
